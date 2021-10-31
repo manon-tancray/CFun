494b79b9cef1b6e03a90a97295c33bfacf887dc5
manon <tancraymanon@gmail.com>
2021-10-31 12:35:06 +0100

    test unitaire Arrivee/génération bdd

--- testprojet_cfun/src/test/java/com/cfun/app/testing/ChoixCouleurTest.java
+++ testprojet_cfun/src/test/java/com/cfun/app/testing/ChoixCouleurTest.java
@@ -1,6 +1,7 @@
 package com.cfun.app.testing;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import com.cfun.app.ChoixCouleur;
 import com.cfun.app.Couleur;
